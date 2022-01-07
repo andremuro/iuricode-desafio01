0033ba580bd36b9efcce4db40e042d8a0653baa3
andremuro <andrefelipe.muro@gmail.com>
2022-01-06 22:33:59 -0300

    Feature: Data colorida

--- js/main.js
+++
@@ -1,9 +0,0 @@
-let temp = setTimeout(function () {
-  const iconSvg = document.querySelectorAll('.svg')
-
-  iconSvg.forEach(function (icon) {
-    icon.addEventListener('click', function () {
-      icon.classList.toggle('active')
-    })
-  })
-}, 500)
